b03bbaad6dfb5ab53bf3410edb1997966d26a5f5
Genildo_Corp <suporteti@sicoobcooprem.com.br>
2026-01-15 08:37:27 -0300

    consertado o products

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,11 @@
 import { Product } from './types';
 
+
 export const PRODUCTS: Product[] = [
   Product.Consorcio,
   Product.Seguro,
-  Product.Investimentos,
+  Product.RDC,
+  Product.LCA,
   Product.Credito,
   Product.Previdencia,
   Product.Compliance,
